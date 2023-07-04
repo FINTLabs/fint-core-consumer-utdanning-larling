c9999c1f9bcc5e65e18b05286f458f614f90f061
Hknots <Hknots99@gmail.com>
2023-07-04 11:41:51 +0200

    copy configuration from other consumers

--- src/main/java/no/fintlabs/consumer/config/LinkMapper.java
+++ src/main/java/no/fintlabs/consumer/config/LinkMapper.java
@@ -8,8 +8,8 @@ public class LinkMapper {
 
     public static Map<String, String> linkMapper() {
         return ImmutableMap.<String, String>builder()
-                .put("no.fint.model.felles.Virksomhet", "/utdanning/larling/virksomhet")
-                .put("no.fint.model.felles.Person", "/utdanning/larling/person")
+                .put("no.fint.model.utdanning.larling.Virksomhet", "/utdanning/larling/virksomhet")
+                .put("no.fint.model.utdanning.larling.Person", "/utdanning/larling/person")
                 .put("no.fint.model.utdanning.larling.Larling", "/utdanning/larling/larling")
                 .build();
     }
