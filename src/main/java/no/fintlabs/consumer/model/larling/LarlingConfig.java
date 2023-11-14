b3cea661f6ef58893927aa6c133936c539ffa504
Hknots <Hknots99@gmail.com>
2023-11-14 13:26:59 +0100

    fix sharedprops reference

--- src/main/java/no/fintlabs/consumer/model/larling/LarlingConfig.java
+++ src/main/java/no/fintlabs/consumer/model/larling/LarlingConfig.java
@@ -1,7 +1,7 @@
 package no.fintlabs.consumer.model.larling;
 
 import no.fint.model.resource.utdanning.larling.LarlingResource;
-import no.fintlabs.core.consumer.shared.ConsumerProps;
+import no.fintlabs.core.consumer.shared.config.ConsumerProps;
 import no.fintlabs.core.consumer.shared.resource.ConsumerConfig;
 import org.springframework.stereotype.Component;
 
